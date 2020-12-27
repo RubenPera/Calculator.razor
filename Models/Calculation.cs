d5d18c1813a9b02ee9bd7d96d1bc5d30fbc4a716
Ruben Pera <r.perag@gmail.com>
2020-12-27 19:39:21 +0100

    Can read input change

--- Models/Calculation.cs
+++ Models/Calculation.cs
@@ -8,6 +8,6 @@ namespace Calculator.Models
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
 
-        public List<CalculationEntry> Entries { get; set; }
+        public List<CalculationEntry> Entries { get; set; } = new List<CalculationEntry>();
     }
 }
